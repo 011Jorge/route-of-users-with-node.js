545c35146d21eebcccde751a8a33c6537ccaf90a
011Jorge <vicentejorge187@gmail.com>
2021-12-06 21:14:31 -0300

    Middlewares add

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ app.use(express.json())
 const port = 3000
 const users = []
 
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" })
+    }
+
+    request.userIndex = index
+    request.userID = id
+
+    next()
+}
+
 app.get('/users', (request, response) => {
     return response.json(users);
 })
@@ -20,30 +35,20 @@ app.post('/users', (request, response) => {
     return response.status(201).json(users);
 })
 
-app.put('/users/:id', (request, response) => {
-    const { id } = request.params
+app.put('/users/:id', checkUserId, (request, response) => {
     const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userID
 
     const updateUser = { id, name, age }
-    const index = users.findIndex(user => user.id === id)
-
-    if (index < 0) {
-        return response.status(404).json({ message: "User not found" })
-    }
 
     users[index] = updateUser
 
     return response.json(updateUser);
 })
 
-app.delete('/users/:id', (request, response) => {
-    const { id } = request.params
-
-    const index = users.findIndex(user => user.id === id)
-
-    if (index < 0) {
-        return response.status(404).json({ message: " User not found" })
-    }
+app.delete('/users/:id', checkUserId, (request, response) => {
+    const index = request.userIndex
 
     users.splice(index, 1)
 
